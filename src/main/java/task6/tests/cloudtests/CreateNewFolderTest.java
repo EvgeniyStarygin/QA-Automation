ba3993ce3cd37ba761ef9720c93fa402de3a82c1
Evgeniy <zzzzz-123@inbox.ru>
2020-07-28 00:28:28 +0300

    add geckodriver.exe

--- src/main/java/task6/tests/cloudtests/CreateNewFolderTest.java
+++ src/main/java/task6/tests/cloudtests/CreateNewFolderTest.java
@@ -1,18 +1,13 @@
 package task6.tests.cloudtests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import task6.businessobjects.Email;
-import task6.businessobjects.User;
 import task6.businessobjects.UserFactory;
 import task6.random.RandomGenerator;
 import task6.screens.CloudMainPage;
 import task6.services.LoginService;
 
 public class CreateNewFolderTest {
-
-    private static CloudMainPage cloudMainPage;
 
     @BeforeMethod
     public void loginToCloud() {
@@ -22,12 +17,10 @@ public class CreateNewFolderTest {
     @Test
     public void createNewFolderTest() {
         String folderName = RandomGenerator.generateRandomFolderName();
-        cloudMainPage = new CloudMainPage();
-        cloudMainPage
+        new CloudMainPage()
                 .clickCreateButton()
                 .clickCreateNewFolderButton()
                 .typeNewFolderName(folderName)
                 .clickConfirmationButton();
-
     }
 }
